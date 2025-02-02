d3f41c018e6c121858bdc9f80d3b8c2271ea5b75
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-02-01 20:52:31 -0500

    feat: Add user registration with validations, create custom error, add global error handler

---
+++ errors/ApiErrors.js
@@ -0,0 +1,9 @@
+class ApiException extends Error {
+  constructor(message, status) {
+    super(message);
+    this.status = status;
+    this.name = "ClimbTrackerApiException";
+  }
+}
+
+export { ApiException };
