7c1171cb19c90d8795ecbc0326b6a6b56c8bbb07
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-02-01 23:25:57 -0500

    feat: Fix user validations using express validator, add friendlier error messages, and add check constraints for password, first name, and last name fields in user table

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,17 +2,15 @@ import * as UserController from "../controllers/UserController.js";
 import {
   registrationValidations,
   loginValidations,
-  validateRequest,
+  withValidation,
 } from "../validations/UserValidations.js";
 import { Router } from "express";
 
 const UserRouter = Router();
 
-UserRouter.use(validateRequest);
-
 UserRouter.post(
   "/register",
-  registrationValidations,
+  withValidation(registrationValidations),
   UserController.registerUser,
 );
 
