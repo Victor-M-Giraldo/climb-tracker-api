0013cd8bb88051b720e632a3e588e1c33e20fc46
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-02-11 20:02:12 -0500

    add hero and react router.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,36 @@ import Footer from "./Footer"
 
 function App() {
   return (
-    <div className="flex flex-col h-full">
+    <div className='flex flex-col h-full'>
       <Header></Header>
-      <main className="flex-1">
-
+      <main className='flex-1 p-4'>
+        <div className='max-w-7xl mx-auto prose grid lg:grid-cols-2 gap-2'>
+          <div>
+            <h1 className='text-5xl'>
+              Track your Climbs, Crush Your Goals, - All in One Place.
+            </h1>
+            <p>
+              Log your bouldering climbs, analyze your progress, and connect
+              with a community of climbers.
+            </p>
+            <p>
+              Whether you're a beginner tracking your first sends or a seasoned
+              climber analyzing your progress, Boulder Log makes it easy to log
+              climbs, set goals, and stay motivated. Join a community of
+              climbers who are pushing their limits and celebrating every send!
+            </p>
+            <button className='btn text-lg'>Sign Up for Free!</button>
+          </div>
+          <img
+            src='/climbing.jpg'
+            alt=''
+            className='mt-0'
+          />
+        </div>
       </main>
       <Footer></Footer>
     </div>
-  )
+  );
 }
 
 export default App
