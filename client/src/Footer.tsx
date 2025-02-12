b45be522666f164981a6359f6b698047c1adc5ad
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-02-11 19:23:46 -0500

    Add footer component

---
+++ client/src/Footer.tsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className='p-4 bg-base-200'>
+        <div className='max-w-7xl m-auto text-center'>
+          <p>
+            <a href='https://github.com/Victor-M-Giraldo/Boulder-Log' className="link">
+              GitHub Repository
+            </a>
+          </p>
+          <p>
+            {new Date().getFullYear()} -{' '}
+            <a href='' className='link'>
+              Victor Giraldo
+            </a>
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
