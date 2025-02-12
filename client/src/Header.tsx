e3a2195ea8263ee6317bacf982b993f1893c11a1
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-02-11 19:23:37 -0500

    Add header component

---
+++ client/src/Header.tsx
@@ -0,0 +1,21 @@
+export default function Header() {
+    return (
+      <>
+        <header className='p-4 bg-base-200'>
+          <div className='max-w-7xl flex m-auto'>
+            <h1 className='text-3xl'>Boulder Log</h1>
+            <nav className='ml-auto flex items-center'>
+              <ul className='flex gap-1'>
+                <a href='' className='hover:link'>
+                  <li>View Climbs</li>
+                </a>
+                <a href='' className='hover:link'>
+                  <li>New Climb</li>
+                </a>
+              </ul>
+            </nav>
+          </div>
+        </header>
+      </>
+    );
+}
