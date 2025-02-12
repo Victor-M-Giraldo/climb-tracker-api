2553217fba6f67c75e1d057a623b277eaf5fa5f1
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-02-12 13:36:28 -0500

    Add layout component, set up react browser router.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,22 @@
-import Header from './Header';
-import Footer from './Footer';
-import Hero from './Hero';
-import Features from './Features';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import Layout from './Layout';
+import HomePage from './HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        element: <HomePage/>,
+        path: '/'
+      }
+    ]
+    },
+])
 
 function App() {
-  return (
-    <div className='flex flex-col h-full'>
-      <Header></Header>
-      <main className='flex-1 p-4 bg-base-200'>
-        <Hero></Hero>
-        <Features></Features>
-      </main>
-      <Footer></Footer>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
