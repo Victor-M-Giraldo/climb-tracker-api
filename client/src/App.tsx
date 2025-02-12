fb14fc2398538021be0e3f40f183f8b9f4b59b39
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-02-12 11:51:27 -0500

    add hero component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,17 @@
-import Header from "./Header"
-import Footer from "./Footer"
+import Header from './Header';
+import Footer from './Footer';
+import Hero from './Hero';
 
 function App() {
   return (
     <div className='flex flex-col h-full'>
       <Header></Header>
       <main className='flex-1 p-4'>
-        <div className='max-w-7xl mx-auto prose grid lg:grid-cols-2 gap-2'>
-          <div>
-            <h1 className='text-5xl'>
-              Track your Climbs, Crush Your Goals, - All in One Place.
-            </h1>
-            <p>
-              Log your bouldering climbs, analyze your progress, and connect
-              with a community of climbers.
-            </p>
-            <p>
-              Whether you're a beginner tracking your first sends or a seasoned
-              climber analyzing your progress, Boulder Log makes it easy to log
-              climbs, set goals, and stay motivated. Join a community of
-              climbers who are pushing their limits and celebrating every send!
-            </p>
-            <button className='btn text-lg'>Sign Up for Free!</button>
-          </div>
-          <img
-            src='/climbing.jpg'
-            alt=''
-            className='mt-0'
-          />
-        </div>
+        <Hero></Hero>
       </main>
       <Footer></Footer>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ client/src/Hero.tsx
@@ -0,0 +1,37 @@
+export default function Hero() {
+    return (
+      <>
+        <div className='max-w-7xl mx-auto prose grid md:grid-cols-2 gap-4'>
+          <div>
+            <h1 className='text-5xl'>
+              Track your Climbs, Crush Your Goals, - All in One Place.
+            </h1>
+            <p>
+              Log your bouldering climbs, analyze your progress, and connect
+              with a community of climbers.
+            </p>
+            <p>
+              Whether you're a beginner tracking your first sends or a seasoned
+              climber analyzing your progress, Boulder Log makes it easy to log
+              climbs, set goals, and stay motivated. Join a community of
+              climbers who are pushing their limits and celebrating every send!
+            </p>
+            <button className='btn text-md'>Sign Up for Free!</button>
+          </div>
+          <div className='min-h-[650px] aspect-[2/3] relative not-prose max-w-full overflow-hidden'>
+            <figure className='bg-[url(/climbing.jpg)] h-full bg-center bg-cover'>
+              <figcaption className='absolute bottom-0 left-[50%] transform -translate-x-1/2 w-full text-center'>
+                <a
+                  href='https://www.pexels.com/photo/a-shirtless-man-in-black-shorts-rubbing-hands-with-powder-6701466/'
+                  className='link'
+                  hrefLang='en'
+                  target='_blank'>
+                  Photo by cottonbro studio from Pexels
+                </a>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </>
+    );
+}
