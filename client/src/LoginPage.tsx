b7de72a52b7781bce53deb65d626e74cdd2b4719
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-02-14 15:58:04 -0500

    Start working on the log in page.

---
+++ client/src/LoginPage.tsx
@@ -0,0 +1,11 @@
+export default function LoginPage() {
+    return (
+        <>
+            <section className="h-full grid place-items-center">
+                <div>
+                    <h1>Log In</h1>
+                </div>
+            </section>
+        </>
+    )
+}
