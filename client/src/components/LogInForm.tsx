80c08eaa510e466f20d63bfd5f576e1e7e0454b6
Victor Giraldo <victor.giraldomestrada@gmail.com>
2025-02-22 23:58:50 -0500

    Start refactoring log in form to use new components

--- client/src/components/LogInForm.tsx
+++ client/src/components/LogInForm.tsx
@@ -1,103 +1,58 @@
 import InputField from './InputField';
 import Form from './Form';
-import { useState } from 'react';
+import Button from './Button';
+import GeneralError from './GeneralError';
+import AuthFooter from './AuthFooter';
 import { useLogin } from '../hooks/useLogin';
-import { handleChange } from '../utils/formHandlers';
-import { validateEmail, validatePassword } from '../utils/validations';
 
 export default function LogInForm() {
   const { login, error: serverError, loading } = useLogin();
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
-
-  const [validationErrors, setValidationErrors] = useState({
-    email: '',
-    password: '',
-  });
-
-  const { email, password } = formData;
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
-    setValidationErrors({ email: '', password: '' });
-
-    const emailValidation = validateEmail(email);
-    const passwordValidation = validatePassword(password);
-
-    if (emailValidation.email || passwordValidation.password) {
-      setValidationErrors({
-        ...emailValidation,
-        ...passwordValidation,
-      });
-      return;
-    }
-
-    login(email, password);
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
   }
 
   return (
     <>
-      <h1 className='text-3xl font-bold text-center mb-6'>Log In</h1>
-      <Form onSubmit={handleSubmit}>
-        <div>
-          <InputField
-            type='email'
-            value={email}
-            name='email'
-            onChange={(e) => handleChange(e, setFormData, formData)}
-            label='Email'
-            placeholder='Enter your email'
-            error={validationErrors.email || serverError?.email}
-            onBlur={(e) => {
-              setValidationErrors({
-                ...validationErrors,
-                ...validateEmail(e.target.value),
-              });
-            }}
-          />
-        </div>
+      <Form
+        onSubmit={handleSubmit}
+        // Wrap it in an anonymous function since it expects an ElementType.
+        // ElementType is something that can be rendered later by calling the function
+        // As opposed to a prerendered element which is the restul of acalling a component type/writing JSX
+        // We would need to use ReactNode for above approach.
+        FormHeader={() => (
+          <h1 className='text-3xl font-bold text-center mb-6'>Log In</h1>
+        )}>
+        <InputField
+          type='email'
+          name='email'
+          placeholder='Enter your email'
+          label='Email'
+          error={serverError?.email}
+          required={true}
+          Wrapper='div'
+        />
 
-        <div>
-          <InputField
-            type='password'
-            value={password}
-            name='password'
-            onChange={(e) => handleChange(e, setFormData, formData)}
-            label='Password'
-            placeholder='Enter your password'
-            error={validationErrors.password || serverError?.password}
-            onBlur={(e) => {
-              setValidationErrors({
-                ...validationErrors,
-                ...validatePassword(e.target.value),
-              });
-            }}
-          />
-        </div>
+        <InputField
+          type='password'
+          name='password'
+          label='Password'
+          placeholder='Enter your password'
+          error={serverError?.password}
+          required={true}
+          Wrapper='div'
+        />
 
-        {serverError?.general && (
-          <div className='text-red-400 text-sm mb-4'>{serverError.general}</div>
-        )}
-        <div>
-          <button
-            type='submit'
-            className='btn btn-primary w-full'
-            disabled={loading}>
-            {loading ? 'Logging in...' : 'Log In'}
-          </button>
-        </div>
+        <GeneralError error={serverError.general} />
+        <Button type='submit' disabled={loading} Wrapper='div'>
+          {loading ? 'Logging in...' : 'Log In'}
+        </Button>
       </Form>
-      <div className='mt-6 text-center'>
-        <p className='text-sm'>
-          Don't have an account?{' '}
-          <a href='/register' className='link text-blue-500 hover:text-blue-600'>
-            Sign up here
-          </a>
-        </p>
-      </div>
+      <AuthFooter promptText="Don't have an account? " link='/register' linkText='Sign up here'/>
     </>
   );
 }
